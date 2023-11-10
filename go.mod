5b41526f79e43dc93dd25e5ffc34df352914cf53
taoshumin_vendor <tsmidx@gmail.com>
2023-11-10 16:22:06 +0800

    feat: 添加测试用例

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/filestype
 go 1.20
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	golang.org/x/net v0.17.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ images_test.go
@@ -0,0 +1,31 @@
+/*
+Copyright 2022 The deepauto-io LLC.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package filestype
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestDetectImageType(t *testing.T) {
+	data, err := os.ReadFile("/Users/taoshumin_vendor/go/src/github.com/filestype/148571693635310_.pic_hd.jpg")
+	assert.NoError(t, err)
+
+	ty := DetectImageType(data)
+	t.Log(ty)
+}
